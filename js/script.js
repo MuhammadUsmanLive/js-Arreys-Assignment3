9dee5bf9b921f02d0c4fc78a3296ba246acf3c27
MuhammadUsmanLive <161132483+MuhammadUsmanLive@users.noreply.github.com>
2024-04-01 17:42:51 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,80 @@
+function anAlert() {
+    alert("This is an alert! (Assignment-JS-3 By Muhammad Usman 205572 )");
+  }
+  function prName() {
+    let myname = document.getElementById("input").value;
+    document.getElementById("statement").innerHTML =
+      "Please! Enter your Name in Input field.";
+  
+    if (myname == "") {
+      alert("Please! Enter your Name in Input field");
+    } else {
+      document.getElementById("output").innerHTML = myname;
+    }
+  }
+  
+  let cities = [
+    "Faisalabad",
+    "Lahore",
+    "Karachi",
+    "Sialkot",
+    "Multan",
+    "Islamabad",
+  ];
+  
+  function prCities() {
+    document.getElementById("statement").innerHTML = "";
+  
+    for (let index = 0; index < cities.length; index++) {
+      document.getElementById("statement").innerHTML +=
+        index + 1 + ")" + cities[index] + "&ensp;";
+    }
+  
+    document.getElementById("output").innerHTML = " ";
+    for (let index = 0; index < cities.length; index++) {
+      document.getElementById("output").innerHTML +=
+        index + ")" + cities[index] + "<br>";
+    }
+  }
+  
+  function adCities() {
+    if (document.getElementById("input").value == "") {
+      alert("please Enter city");
+    } else {
+      cities.push(document.getElementById("input").value);
+    }
+  }
+  function rmCities() {
+    
+      cities.pop(document.getElementById("input").value);
+    }
+  
+  
+  function gTable() {
+    let num = +prompt("Table of which Number");
+  
+    if (!isNaN(num)) {
+      document.getElementById("output").innerHTML =
+        "<h2 class='text-primary'>Table of" + " " + num + "</h2>";
+  
+      for (let i = 1; i <= 10; i++) {
+        document.getElementById("output").innerHTML +=
+          num + " * " + i + " = " + num * i + "<br>";
+      }
+    } else {
+      alert("please! enter a number");
+    }
+  }
+  
+  
+  function clearop() {
+      
+      document.getElementById("statement").innerHTML = " ";
+      document.getElementById("output").innerHTML = " ";
+  }
+  
+  function clearinp() {
+    document.getElementById("input").value = " ";
+    document.getElementById("statement").innerHTML = " ";
+  }
+  
